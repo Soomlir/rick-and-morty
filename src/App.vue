7b8b62eca531f82c4c26463d711f412ddaa88b94
Soomlir <Soomlir@yandex.ru>
2024-06-27 21:27:35 +0300

    code edits

--- src/App.vue
+++ src/App.vue
@@ -2,71 +2,62 @@
 import { ref } from "vue";
 
 let charactersData = ref([]);
-let page = ref(1); // Изменяем начальное значение страницы на 1
+let page = ref(1);
 const disabledPrev = ref(false);
 const disabledNext = ref(false);
 const searchItem = ref("");
 
-const fetchData = (pageNumber) => {
-  fetch(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`)
+const getData = (page) => {
+  fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
     .then((response) => response.json())
     .then((data) => {
       charactersData.value = data.results;
     });
 };
 
-fetchData(page.value); // Вызываем fetchData для загрузки данных при инициализации
+getData(page.value); // Вызываем getData для загрузки данных при инициализации
 
 const nextPage = () => {
   if (page.value < 42) {
-    // Проверяем, что значение страницы меньше 42
-    page.value++;
-    fetchData(page.value);
+    getData(page.value++);
   }
 };
 
 const prevPage = () => {
   if (page.value > 1) {
-    page.value--; // Уменьшаем значение страницы на 1, если текущая страница больше 1
-    fetchData(page.value); // Вызываем fetchData с новым значением страницы
+    getData(page.value--);
   }
 };
-
-const filteredData = (searchItem) => {
-  fetch(
-    `https://rickandmortyapi.com/api/character/?name=${searchItem}&status=alive`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      charactersData.value = data.results;
-    });
-};
-}
 </script>
 
 <template>
   <div class="wrapper">
     <h1 class="title">The Rick and Morty API</h1>
-    <div class="buttons">
-      <button @click="prevPage" :disabled="disabledPrev">PREV</button>
-      <span>{{ page }}</span>
-      <button @click="nextPage" :disabled="disabledNext">NEXT</button>
-    </div>
-    <div>
-      <input type="search" v-model="searchItem" />
-      <button @click="filteredData">Искать</button>
+    <div class="controls-wrapper">
+      <div class="buttons">
+        <button @click="prevPage" :disabled="disabledPrev">PREV</button>
+        <span>{{ page }}</span>
+        <button @click="nextPage" :disabled="disabledNext">NEXT</button>
+      </div>
+      <div>
+        <input type="search" v-model="searchItem" />
+        <button @click="filteredData">Искать</button>
+      </div>
     </div>
     <ul class="character-list">
       <li class="character-item" v-for="character of charactersData">
-        <img :src="character.image" alt="..." width="230" height="260" />
-        <div class="info">
-          <h2>{{ character.name }}</h2>
-          <p>{{ character.status }} - {{ character.species }}</p>
-          <p>Last known location:</p>
-          <p>{{ character.origin.name }}</p>
-          <p>First seen in:</p>
-          <p>{{ character.episode[0] }}</p>
-        </div>
+        <img :src="character.image" alt="..." width="230" height="220" />
+        <dl class="info">
+          <dt class="info__name">{{ character.name }}</dt>
+          <dd class="info__status">
+            <span class="info__status-indicator"></span>{{ character.status }} -
+            {{ character.species }}
+          </dd>
+          <dt class="info__location">Last known location:</dt>
+          <dd>{{ character.origin.name }}</dd>
+          <dt class="info__seen">First seen in:</dt>
+          <dd>{{ character.episode[0] }}</dd>
+        </dl>
       </li>
     </ul>
   </div>
@@ -74,16 +65,52 @@ const filteredData = (searchItem) => {
 
 <style>
 body {
-  background-color: lightgray;
+  background: rgb(32, 35, 41);
+}
+
+button {
+  color: #ffffff;
+  background: rgb(60, 62, 68);
+  border: none;
+  cursor: pointer;
+  width: 100px;
+  height: 40px;
+  margin: 10px;
+}
+
+input[type="search"] {
+  width: 200px;
+  height: 35px;
+  padding: 5px 10px;
 }
 
 .wrapper {
-  width: 960px;
+  max-width: 1440px;
   margin-right: auto;
   margin-left: auto;
 }
 
 .title {
+  text-align: center;
+  color: #ffffff;
+  font-weight: 900;
+  z-index: 1;
+}
+
+.controls-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.buttons {
+  color: #ffffff;
+  font-size: 30px;
+  padding: 10px;
+}
+
+.buttons span {
+  width: 30px;
+  display: inline-block;
   text-align: center;
 }
 
@@ -91,14 +118,64 @@ body {
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 30px;
+  width: 1300px;
 }
+
 .character-item {
   display: flex;
   gap: 30px;
   color: #ffffff;
-  background-color: gray;
+  background-color: rgb(60, 62, 68);
+  width: 600px;
+  height: 220px;
+  display: flex;
+  overflow: hidden;
+  background: rgb(60, 62, 68);
+  border-radius: 0.5rem;
+  margin: 0.75rem;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+    rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
 }
+
+.character-item img {
+  margin: 0px;
+  opacity: 1;
+  transition: opacity 0.5s;
+}
+
 .info {
-  min-height: 230px;
+  margin: 0;
+  padding: 0;
+  padding: 7px;
+  font-size: 20px;
+}
+
+.info dd {
+  margin: 0;
+  margin-bottom: 20px;
+}
+
+.info__name {
+  font-size: 2.5rem;
+}
+
+.info__status {
+  font-size: 16px;
+}
+
+.info__status-indicator {
+  height: 0.5rem;
+  width: 0.5rem;
+  margin-right: 0.375rem;
+  background: rgb(85, 204, 68);
+  border-radius: 50%;
+  display: inline-block;
+}
+
+.info__location,
+.info__seen {
+  color: rgb(158, 158, 158);
+  font-size: 16px;
+  font-weight: 500;
 }
 </style>
